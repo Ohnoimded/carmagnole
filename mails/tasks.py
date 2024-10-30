e02cdcb4d018438b37bf9d96bd3182ecb0bf89cb
Ohnoimded <88839014+Ohnoimded@users.noreply.github.com>
2024-10-30 12:14:34 +0530

    asynched mail sending

--- mails/tasks.py
+++ mails/tasks.py
@@ -14,50 +14,52 @@ import time
 redis_client = redis.Redis(connection_pool=settings.REDIS["default"]["POOL"],encoding='utf-8',decode_responses=True)
 
 @shared_task
-def send_newsletter():
+def send_email_task(subject, body, from_email, to_email):
+    try:
+        email = EmailMultiAlternatives(
+            subject=subject,
+            body=body,  # Plain text content
+            from_email=from_email,
+            to=[to_email],
+        )
+        email.attach_alternative(body, "text/html")
+        email.send()
+    except Exception as e:
+        print(f"Failed to send email to {to_email}: {e}")
 
+@shared_task
+def send_newsletter():
     html_content = redis_client.get('daily_newsletter')
     if not html_content:
         create_mail(pls_return=False)
         html_content = redis_client.get('daily_newsletter')
-        
+
     if isinstance(html_content, bytes):
         html_content = html_content.decode('utf-8')
-    
-    while True:
-        
-        last_id = redis_client.get('sent_newsletter_subscribers_last_id')
-        
-        if last_id:
-            last_id = int(last_id)
-            subscribers = NewsletterSubscriberModel.objects.all().\
-            filter(subscribed=True, id__gt=last_id).order_by('id')[:10]
-        else:
-            subscribers = NewsletterSubscriberModel.objects.all().filter(subscribed = True).order_by('id')[:10]
-        
-        if not subscribers:
-            break
-        
-        try:
-            for subscriber in subscribers:
-                
-                if redis_client.sismember('sent_newsletter_subscribers', subscriber.id):
-                    continue
-                email = EmailMultiAlternatives(
-                    subject="Le Carmagnole: Daily Nuggets",
-                    body="Today's Top News",  # Plain text content
-                    from_email='Le Carmagnole <noreply@carmagnole.ohnoimded.com>',
-                    to=[subscriber.email],
-                )
-                email.attach_alternative(html_content, "text/html")
-                email.send()
-                
-                redis_client.sadd('sent_newsletter_subscribers', subscriber.id)
-                redis_client.expire('sent_newsletter_subscribers', 60**60*6) 
-                
-            last_subscriber_id = subscribers.last().id
+
+    last_id = redis_client.get('sent_newsletter_subscribers_last_id')
+    if last_id:
+        last_id = int(last_id)
+        subscribers = NewsletterSubscriberModel.objects.filter(subscribed=True, id__gt=last_id).order_by('id')[:10]
+    else:
+        subscribers = NewsletterSubscriberModel.objects.filter(subscribed=True).order_by('id')[:10]
+
+    if not subscribers:
+        return  
+
+    for subscriber in subscribers:
+        # It is memory heavy to add to set, if no. of subscribers grow. But SES 200 per day limit ruins the fun of destroying the container with memory fill
+        if not redis_client.sismember('sent_newsletter_subscribers', subscriber.id): 
+            
+            last_subscriber_id = subscriber.id
+            redis_client.sadd('sent_newsletter_subscribers', last_subscriber_id)
             redis_client.set('sent_newsletter_subscribers_last_id', last_subscriber_id)
-            time.sleep(1)
-        except:
-            pass
-        
+            
+            send_email_task.delay(
+                subject="Le Carmagnole: Daily Nuggets",
+                body=html_content,
+                from_email='Le Carmagnole <noreply@carmagnole.ohnoimded.com>',
+                to_email=subscriber.email,
+            )
+
+    
